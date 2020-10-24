6ceb1b9775cfd4971eaade75fc8973c6452924c1
Sam Morris <0v3rdr0n3@gmail.com>
2020-10-24 13:56:48 -0400

    sfs-recoll: Add font-locking for properties buffer

--- sfs-recoll.el
+++ sfs-recoll.el
@@ -24,10 +24,15 @@
 (require 'ivy)
 (require 'dired)
 
+(defvar sfs-dired-mode-map)
+(setq sfs-dired-mode-map
+      (let ((map (make-sparse-keymap)))
+        (define-key map (kbd "<C-return>") 'sfs-display-info)
+        ;; (define-key map (kbd "q") 'quit-window)
+        map))
 (define-minor-mode sfs-dired-mode
-  "Get your foos in the right places."
-  :lighter " sfs-dired"
-  :keymap (make-sparse-keymap))
+  "Minor mode for sfs results in dired."
+  :lighter " sfs-dired")
 
 (defvar dbIndex nil)
 (make-variable-buffer-local 'dbIndex)
@@ -71,7 +76,7 @@
       (display-buffer buffer)))
   (select-window (get-mru-window)))
 
-(defun sfs-display-info ()
+(defun sfs-display-info-minibuffer ()
   "Display a tooltip showing metadata about the file at POINT."
   (interactive)
   (let (entry)
@@ -79,6 +84,75 @@
     (minibuffer-with-setup-hook
         (lambda () (sfs-display-info-hook entry))
       (read-string "Enter: "))))
+
+(defvar sfs--recoll-fields)
+(setq sfs--recoll-fields
+      '("rcludi"
+        "fbytes"
+        "dbytes"
+        "sig"
+        "url"
+        "abstract"
+        "filename"
+        "relevancyrating"
+        "mtype"
+        "origcharset"
+        "mtime"
+        "fmtime"
+        "pcbytes"
+        ))
+(defvar sfs-properties-highlights nil "First element for `font-lock-defaults'.")
+(dolist (field sfs--recoll-fields)
+  (add-to-list 'sfs-properties-highlights `(,field . font-lock-keyword-face)))
+
+(defvar sfs-properties-mode-map)
+(setq sfs-properties-mode-map
+      (let ((map (make-sparse-keymap)))
+        (define-key map (kbd "q") 'quit-window)
+        map))
+(define-derived-mode sfs-properties-mode fundamental-mode "SFS Properties"
+  "Major mode for SFS interactive query editor.
+
+The query editor uses the Recoll query language, documented here:
+https://www.lesbonscomptes.com/recoll/usermanual/webhelp/docs/RCL.SEARCH.LANG.html"
+  ;; (setq font-lock-keywords sfs--properties-highlights)
+  ;; (setq font-lock-defaults '(sfs--recoll-fields))
+  (setq font-lock-defaults '(sfs-properties-highlights))
+  )
+
+(defun sfs-display-info ()
+  "Display a tooltip showing metadata about the file at POINT."
+  (interactive)
+  (let ((entry (assoc (dired-get-filename) dbIndex))
+        (buffer (get-buffer-create "*SFS Properties*"))
+        str)
+    (setq str (sfs-recoll-build-tooltip entry))
+    (with-current-buffer buffer
+      (visual-line-mode 1)
+      ;; (auto-fill-mode 1)
+      (let ((inhibit-read-only t))
+        (erase-buffer)
+        (insert str))
+      (sfs-properties-mode)
+      (display-buffer buffer)
+      )))
+
+;; (defun sfs-display-info ()
+;;   "Display a tooltip showing metadata about the file at POINT."
+;;   (interactive)
+;;   (let ((buffer (get-buffer-create "*SFS Properties*"))
+;;         entry
+;;         str)
+;;     (setq str (sfs-recoll-build-tooltip entry))
+;;     (with-current-buffer buffer
+;;       (visual-line-mode 1)
+;;       ;; (auto-fill-mode 1)
+;;       (let ((inhibit-read-only t))
+;;         (erase-buffer)
+;;         (insert str))
+;;       (sfs-properties-mode)
+;;       (display-buffer buffer)
+;;       )))
 
 (defmacro minibuffer-quit-and-run (&rest body)
   "Quit the minibuffer and run BODY afterwards."
@@ -158,25 +232,28 @@
   "Query the Recoll database and display the results in dired.
 QUERYSTR is a search string conforming to the Recoll Query Language."
   (interactive "sQuery: ")
-  (let (rawDB db index urls sfs-recollBuf)
-    (setq rawDB (dbus-call-method
-                 :session
-                 "com.sfs.SearchService"
-                 "/SFS"
-                 "com.sfs.SearchInterface"
-                 "Query"
-                 queryStr))
-    (if rawDB
-        (progn (setq db
-                     (remove-if-not #'(lambda (entry) (file-exists-p (sfs-recoll-get-field "url" entry)))
-                                    rawDB))
-               (setq index (sfs-recoll-build-dired-index db))
-               (setq urls (sfs-recoll-collect-fields '("url") db))
-               (setq sfs-recollBuf (dired urls))
-               (set-buffer sfs-recollBuf)
-               (setq dbIndex index)
-               (sfs-dired-mode) )
-      (message "SFS: Couldn't find any results for your search..."))))
+  (if (not (string= queryStr ""))
+      (let (rawDB db index urls sfs-recollBuf)
+        (setq rawDB (dbus-call-method
+                     :session
+                     "com.sfs.SearchService"
+                     "/SFS"
+                     "com.sfs.SearchInterface"
+                     "Query"
+                     queryStr))
+        (if rawDB
+            (progn (setq db
+                         (remove-if-not #'(lambda (entry) (file-exists-p (sfs-recoll-get-field "url" entry)))
+                                        rawDB))
+                   (setq index (sfs-recoll-build-dired-index db))
+                   (setq urls (sfs-recoll-collect-fields '("url") db))
+                   (setq sfs-recollBuf (dired urls))
+                   (set-buffer sfs-recollBuf)
+                   (rename-buffer "*SFS dired*")
+                   (setq dbIndex index)
+                   (sfs-dired-mode) )
+          (message "SFS: Couldn't find any results for your search...")))
+    (message "SFS: query string empty...")))
 
 (defun sfs-recoll-exit ()
   "Exit SFS Recoll server."
