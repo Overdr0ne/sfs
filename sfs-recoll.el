215f04c3de5348be9e7e3dc85c07aba85422ddf2
Sam Morris <0v3rdr0n3@gmail.com>
2020-11-01 14:16:19 -0500

    sfs-recoll: renamed internal functions

--- sfs-recoll.el
+++ sfs-recoll.el
@@ -27,42 +27,36 @@
 (defvar sfs-dired-mode-map)
 (setq sfs-dired-mode-map
       (let ((map (make-sparse-keymap)))
-        (define-key map (kbd "<C-return>") 'sfs-display-info)
-        ;; (define-key map (kbd "q") 'quit-window)
+        (define-key map (kbd "<C-return>") 'sfs-dired-display-info)
         map))
 (define-minor-mode sfs-dired-mode
   "Minor mode for sfs results in dired."
   :lighter " sfs-dired")
 
-(defvar dbIndex nil)
-(make-variable-buffer-local 'dbIndex)
-
-(defun sfs-recoll-get-field (field aList)
+(defun sfs--recoll-get-field (field aList)
   "Utility function to get FIELD from ALIST."
   (cadr (assoc field aList)))
 
-(defun sfs-recoll-get-fields (fields aList)
+(defun sfs--recoll-get-fields (fields aList)
   "Utility function to get FIELDS from ALIST."
-  (mapcar #'(lambda (field) (sfs-recoll-get-field field aList))
+  (mapcar #'(lambda (field) (sfs--recoll-get-field field aList))
           fields))
 
-(defun sfs-recoll-collect-fields (fields db)
+(defun sfs--recoll-collect-fields (fields db)
   "Collect data FIELDS from DB."
-  (mapcar #'(lambda (entry) (car (sfs-recoll-get-fields fields entry)))
+  (mapcar #'(lambda (entry) (car (sfs--recoll-get-fields fields entry)))
           db))
 
-(defun sfs-recoll-build-tooltip (dbEntry)
+(defun sfs--recoll-property-str (entry)
   "Build tooltip string from DBENTRY."
-  (let ((dbInfo nil)
-        (infoStrs nil))
-    (setq dbInfo (cdr dbEntry))
-    (setq dbInfo
+  (let (infoStrs)
+    (setq entry
           (remove-if #'(lambda (field) (string-empty-p (cadr field)))
-                     dbInfo))
+                     entry))
     (setq infoStrs (mapcar
                     #'(lambda (field)
                         (concat (car field) ": " (cadr field) "\n"))
-                    dbInfo))
+                    entry))
     (reduce #'concat infoStrs)))
 
 (defun sfs-display-info-hook (entry)
@@ -75,15 +69,6 @@
       (insert str)
       (display-buffer buffer)))
   (select-window (get-mru-window)))
-
-(defun sfs-display-info-minibuffer ()
-  "Display a tooltip showing metadata about the file at POINT."
-  (interactive)
-  (let (entry)
-    (setq entry (assoc (dired-get-filename) dbIndex))
-    (minibuffer-with-setup-hook
-        (lambda () (sfs-display-info-hook entry))
-      (read-string "Enter: "))))
 
 (defvar sfs--recoll-fields)
 (setq sfs--recoll-fields
@@ -120,39 +105,64 @@ https://www.lesbonscomptes.com/recoll/usermanual/webhelp/docs/RCL.SEARCH.LANG.ht
   (setq font-lock-defaults '(sfs-properties-highlights))
   )
 
-(defun sfs-display-info ()
+(defun sfs--recoll-search-md5 (filename)
+  (let (buf-str query entry)
+    (setq buf-str (with-temp-buffer
+                    (insert-file-contents filename)
+                    (buffer-string)))
+    (setq query (concat "rclmd5:"
+                        (secure-hash 'md5 buf-str)))
+    (car (sfs--recoll-search query))
+    )
+  )
+
+(defun sfs--recoll-search-filename (filename)
+  (let (query split dirs name)
+    (setq split (split-string filename "/"))
+    (setq split (remove-if #'(lambda (str) (string= str ""))
+                           split))
+    (setq name (car (last split)))
+    (setq dirs (butlast split))
+    (setq query (mapcar #'(lambda (dir) (concat "dir:" dir))
+                        dirs))
+    (add-to-list 'query (concat "filename:" name))
+    (setq query (string-join query " AND "))
+    (car (sfs--recoll-search query))
+    )
+  )
+
+(defun sfs--recoll-file-properties (filename)
+  "Get recoll properties for FILENAME by looking up its md5 hash or guessing from the filename."
+  (let (entry)
+    (setq entry
+          (-some #'(lambda (x) x)
+                 `(,(sfs--recoll-search-md5 filename)
+                   ,(progn
+                      (message "Couldn't find entry based on it's hash, guessing the entry based on filename...")
+                      (sfs--recoll-search-filename filename)))))))
+
+(defun sfs-dired-display-info ()
   "Display a tooltip showing metadata about the file at POINT."
   (interactive)
-  (let ((entry (assoc (dired-get-filename) dbIndex))
+  (let ((entry (sfs--recoll-file-properties (dired-get-filename)))
         (buffer (get-buffer-create "*SFS Properties*"))
+        (filename (dired-get-filename))
         str)
-    (setq str (sfs-recoll-build-tooltip entry))
-    (with-current-buffer buffer
-      (visual-line-mode 1)
-      ;; (auto-fill-mode 1)
-      (let ((inhibit-read-only t))
-        (erase-buffer)
-        (insert str))
-      (sfs-properties-mode)
-      (display-buffer buffer)
+    (if entry
+        (progn
+          (setq str (sfs--recoll-property-str entry))
+          (with-current-buffer buffer
+            (visual-line-mode 1)
+            ;; (auto-fill-mode 1)
+            (let ((inhibit-read-only t))
+              (erase-buffer)
+              (insert str))
+            (sfs-properties-mode)
+            (display-buffer buffer)
+            )
+          )
+      (message "This file has not been indexed by Recoll...")
       )))
-
-;; (defun sfs-display-info ()
-;;   "Display a tooltip showing metadata about the file at POINT."
-;;   (interactive)
-;;   (let ((buffer (get-buffer-create "*SFS Properties*"))
-;;         entry
-;;         str)
-;;     (setq str (sfs-recoll-build-tooltip entry))
-;;     (with-current-buffer buffer
-;;       (visual-line-mode 1)
-;;       ;; (auto-fill-mode 1)
-;;       (let ((inhibit-read-only t))
-;;         (erase-buffer)
-;;         (insert str))
-;;       (sfs-properties-mode)
-;;       (display-buffer buffer)
-;;       )))
 
 (defmacro minibuffer-quit-and-run (&rest body)
   "Quit the minibuffer and run BODY afterwards."
@@ -180,53 +190,40 @@ https://www.lesbonscomptes.com/recoll/usermanual/webhelp/docs/RCL.SEARCH.LANG.ht
    (sfs-display-info)))
 ;; (substitute-key-definition 'dired-previous-line 'sfs-dired-previous-line dired-mode-map)
 
-(defun sfs-recoll-tooltip ()
+(defun sfs-dired-tooltip ()
   "Display a tooltip showing metadata about the file at POINT."
   (interactive)
-  (let (entry)
-    (setq entry (assoc (dired-get-filename) dbIndex))
-    (popup-tip (sfs-recoll-build-tooltip entry) :point(line-beginning-position))))
+  (let ((entry (sfs--recoll-file-properties (dired-get-filename))))
+    (popup-tip (sfs--recoll-property-str entry) :point(line-beginning-position))))
 
-(defun sfs-recoll-get-ivy-alist (dbEntry)
-  "Build alist for ivy DBENTRY."
-  (let (dbInfo infoStrs)
-    (setq dbInfo (cdr dbEntry))
-    (setq dbInfo
-          (remove-if #'(lambda (field) (string-empty-p (cadr field)))
-                     dbInfo))
-    (setq infoStrs
-          (mapcar #'(lambda (field) (cons (concat (car field) ": " (cadr field)) (cadr field)))
-                  dbInfo))))
-
-(defun sfs-recoll-dired-ivy ()
+(defun sfs-dired-ivy ()
   "Operate on file metadata at POINT."
   (interactive)
   (let (entry)
     (setq entry
-          (-> (dired-get-filename)
-             (assoc dbIndex)
-             (sfs-recoll-get-ivy-alist)))
+          (sfs--recoll-file-properties (dired-get-filename)))
     (ivy-read "Find Property: " entry
               :caller 'sfs-recoll-dired-ivy
               :action
-              #'(lambda (a) (progn
+              #'(lambda (a) (progn)
                          (kill-new (cdr a))
-                         (print (concat "Entry\n---\n" (cdr a) "\n---\ncopied to kill-ring.")))))))
+                         (print (concat "Entry\n---\n"
+                                        (cdr a)
+                                        "\n---\ncopied to kill-ring."))))))
 
-(defun sfs-recoll-build-dired-index (db)
-  "Build an alist for the DB using the file url as key."
-  (mapcar #'(lambda (entry) (cons (sfs-recoll-get-field "url" entry) entry))
-          db))
-
-(defun sfs-recoll-call (method &rest args)
+(defun sfs--recoll-call (methodStr &rest args)
   "Send METHOD to the SFS python server with optional ARGS."
+  ;; TODO only works with one argument
   (dbus-call-method
    :session
-   "com.sfs-recoll.SearchService"
+   "com.sfs.SearchService"
    "/SFS"
-   "com.sfs-recoll.SearchInterface"
-   method
-   args))
+   "com.sfs.SearchInterface"
+   methodStr
+   (car args)))
+
+(defun sfs--recoll-search (queryStr)
+  (sfs--recoll-call "Query" queryStr))
 
 (defun sfs-recoll (queryStr)
   "Query the Recoll database and display the results in dired.
@@ -234,24 +231,19 @@ QUERYSTR is a search string conforming to the Recoll Query Language."
   (interactive "sQuery: ")
   (if (not (string= queryStr ""))
       (let (rawDB db index urls sfs-recollBuf)
-        (setq rawDB (dbus-call-method
-                     :session
-                     "com.sfs.SearchService"
-                     "/SFS"
-                     "com.sfs.SearchInterface"
-                     "Query"
-                     queryStr))
+        (setq rawDB (sfs--recoll-search queryStr))
         (if rawDB
-            (progn (setq db
-                         (remove-if-not #'(lambda (entry) (file-exists-p (sfs-recoll-get-field "url" entry)))
-                                        rawDB))
-                   (setq index (sfs-recoll-build-dired-index db))
-                   (setq urls (sfs-recoll-collect-fields '("url") db))
-                   (setq sfs-recollBuf (dired urls))
-                   (set-buffer sfs-recollBuf)
-                   (rename-buffer "*SFS dired*")
-                   (setq dbIndex index)
-                   (sfs-dired-mode) )
+            (progn
+              (setq db
+                    (remove-if-not #'(lambda (entry)
+                                       (file-exists-p (sfs--recoll-get-field "url"
+                                                                             entry)))
+                                   rawDB))
+              (setq urls (sfs--recoll-collect-fields '("url") db))
+              (setq sfs-recollBuf (dired urls))
+              (set-buffer sfs-recollBuf)
+              (rename-buffer (concat "*SFS dired* : " (buffer-name)))
+              (sfs-dired-mode))
           (message "SFS: Couldn't find any results for your search...")))
     (message "SFS: query string empty...")))
 
