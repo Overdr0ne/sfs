e8bce8b008517f58fd32a6efc3f15f4e463a10cc
Overdr0ne <0v3rdr0n3@gmail.com>
2020-11-01 15:53:44 -0500

    sfs: add top level file to create global configs

---
+++ sfs.el
@@ -0,0 +1,32 @@
+;;; sfs.el --- SFS - Search File-System  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Sam
+
+;; Author: Sam <scmorris.dev@gmail.com>
+;; Keywords: tools, files
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+
+;;; Code:
+
+(require 'sfs-index)
+(require 'sfs-recoll)
+(require 'sfs-tag)
+(require 'sfs-tui)
+
+(provide 'sfs)
+;;; sfs.el ends here
