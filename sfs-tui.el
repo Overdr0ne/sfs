b5494412ddb2f26ce35d7d397822d19b1e624a5e
Overdr0ne <0v3rdr0n3@gmail.com>
2020-11-13 16:39:46 -0500

    tui: pile, allow recs to be saved hierarchically, funify names
    recs can now be saved to.rec.at.path from the recollector. This should
    make it easier to save things between sessions. Also made the
    recollection/rec/recollector terminology in this chunk.

--- sfs-tui.el
+++ sfs-tui.el
@@ -29,15 +29,26 @@
   "Customization group for SFS."
   :group 'Applications)
 
-(defcustom sfs-class-favs nil
-  "An alist to save your favorite SFS queries, associating queries with symbols. These are then used to generate the collector menu, or can be recalled in the SFS query editor."
+(defcustom sfs-favorites nil
+  "An alist to save your favorite SFS queries, associating queries with symbols. These are then used to generate the collector menu, or can be recalled in the recollector."
   :group 'sfs)
+(setq sfs-favorites nil)
+
+(defcustom sfs-recs `((favorites . ,sfs-favorites)
+                      sfs--rec-recents
+                      sfs--rec-media)
+  "A list of set recs to be displayed in the SFS collector TUI."
+  :type  'sexp
+  :group 'sfs)
+(setq sfs-recs `((favorites . ,sfs-favorites)
+                 sfs--rec-recents
+                 sfs--rec-media))
 
 (defun sfs--queries-or (query-list)
   "Create ORed query string from QUERY-LIST."
   (string-join query-list " OR "))
 
-(defun sfs-split-and-prefix (prefix query)
+(defun sfs--split-and-prefix (prefix query)
   "Split the QUERY on space, and prepend the provided PREFIX to each."
   (mapcar #'(lambda (str)
               (concat prefix ":" str))
@@ -56,12 +67,6 @@
 ;;   (interactive)
 ;;   (let (sep)
 ;;     (sfs-recoll (string-join (cdr sfs--query ())))))
-
-(defun sfs--insert-section-heading (heading)
-  "Format and insert HEADING into file."
-  (put-text-property 0 (length heading) 'face 'sfs-heading
-                     heading)
-  (insert heading ":\n"))
 
 (defface sfs-logo
   '((((class grayscale) (background light)) :foreground "LightGray" :weight bold)
@@ -83,7 +88,7 @@ Use `set-region-writeable' to remove this property."
   (with-silent-modifications
     (put-text-property begin end 'read-only t)))
 
-;;; Query Editor
+;;; Recollector
 
 (defvar sfs--query "")
 
@@ -118,34 +123,82 @@ Use `set-region-writeable' to remove this property."
 
 ;; I need explicit function names to make help-modes useful(use which-key and edit names?), this macro automatically creates them to keep fields as single source of truth
 (defmacro sfs--def-prefix-fun (field)
-  "Generate a function for inserting FIELD prefix into the query editor."
+  "Generate a function for inserting FIELD prefix into the recollector."
   `(defun ,(intern (concat "sfs--prefix-" field)) ()
-     ,(concat "Add " field ": prefix in query editor.")
+     ,(concat "Add " field ": prefix in recollector")
      (interactive)
      (insert ,(concat field ":"))))
 (dolist (field (sfs--fields))
   (eval `(sfs--def-prefix-fun ,field)))
 
 (defvar sfs--editor-widget)
-(defun sfs-save-query (id)
-  "Save current query to ID under the sfs-class-favs custom variable."
+(defun sfs-add-favorite (id)
+  "Save current query to ID under the sfs-favorites custom variable."
   (interactive "sEnter ID for query: ")
-  (push `(,id . ,(widget-value sfs--editor-widget)) sfs-class-favs)
-  sfs-class-favs)
+  (sfs-id-add id (widget-value sfs--editor-widget) sfs-favorites)
+  sfs-favorites)
 
-(defun sfs-query-editor-insert-set (set-id)
-  "Insert the given SET-ID into the query editor."
-  (interactive "sEnter a set: ")
-  (widget-insert (cdr (alist-get set-id (sfs--class-all)))))
+(defun sfs--id-get (id rec-tree)
+  (if (and id rec-tree (listp rec-tree))
+      (if (rest id)
+          (sfs--id-get (rest id)
+                       (alist-get (car id) rec-tree))
+        (alist-get (car id) rec-tree))
+    nil))
 
-(defvar sfs-query-editor-mode-map)
-(setq sfs-query-editor-mode-map
+(defun sfs-id-get (id &optional rec-tree)
+  (let ((path (mapcar #'intern
+                      (split-string id "\\."))))
+    (when (not rec-tree) (setq rec-tree sfs-recs))
+    (sfs--id-get path rec-tree)))
+
+(defun sfs--make-nested-rec (id val)
+  (if id
+      (if (rest id)
+          `((,(car id) . ,(sfs--make-nested-rec (rest id) val)))
+        `((,(car id) . ,val)))
+    nil))
+
+(defun sfs--id-add (id val rec-tree)
+  (cond ((not val)
+         (message "SFS: No value provided...")
+         rec-tree)
+        ((not id) ;; just add the leaf
+         (push val rec-tree)
+         rec-tree)
+        ((alist-get (car id) rec-tree) ;; add updated descendents
+         (setf (alist-get (car id) rec-tree)
+               (sfs--id-add (rest id) val
+                            (alist-get (car id) rec-tree)))
+         rec-tree)
+        (t ;; make branch and graft it
+         (push (first (sfs--make-nested-rec id val)) rec-tree)
+         rec-tree)))
+
+(defun sfs-id-add (id val rec-tree)
+  "Set sfs ID path to VAL."
+  (message "-------------------------")
+  (let ((path (mapcar #'intern
+                      (split-string id "\\."))))
+    ;; (when (not rec-tree) (setq rec-tree sfs-recs))
+    (setf rec-tree (sfs--id-add path `(,val) rec-tree))
+    (message "5 %S" rec-tree))
+  (message "7 %S" rec-tree)
+  rec-tree)
+
+(defun sfs-save-query (id)
+  "Save current query to ID under the sfs-favorites custom variable."
+  (interactive "sEnter a path.to.an.id for query: ")
+  (sfs-id-add id (widget-value sfs--editor-widget)))
+
+(defvar sfs-recollector-mode-map)
+(setq sfs-recollector-mode-map
       (let ((map (copy-keymap widget-field-keymap)))
         (define-key map (kbd "<C-return>") 'widget-field-activate)
         (define-key map (kbd "<return>") 'newline-and-indent)
         map))
 (general-define-key
- :keymaps 'sfs-query-editor-mode-map
+ :keymaps 'sfs-recollector-mode-map
  :prefix "C-c"
  "C-a" '(sfs--prefix-author            :wk "prefix author:")
  "C-c" '(sfs--prefix-containerfilename :wk "prefix containerfilename:")
@@ -157,48 +210,24 @@ Use `set-region-writeable' to remove this property."
  "C-r" '(sfs--prefix-recipient         :wk "prefix recipient:")
  "C-l" '(sfs--prefix-rclcat            :wk "prefix rclcat:")
  "C-m" '(sfs--prefix-mime              :wk "prefix mime:")
- "C-o" '(sfs--insert-collection        :wk "insert collection")
+ "C-o" '(sfs--insert-recollection      :wk "insert collection")
  "C-q" '(quit-window                   :wk "quit window")
- "C-s" '(sfs--prefix-size              :wk "prefix size:")
+ "C-s" '(sfs-save-query                :wk "prefix size:")
  "C-t" '(sfs--prefix-date              :wk "prefix date:")
- "C-v" '(sfs-save-query                :wk "save query"))
+ "C-v" '(sfs-add-favorite              :wk "save query")
+ "C-z" '(sfs--prefix-size              :wk "prefix size:"))
+(define-derived-mode sfs-recollector-mode prog-mode "SFS"
+  "Major mode for SFS interactive recollector.
 
-
-(define-derived-mode sfs-query-editor-mode prog-mode "SFS"
-  "Major mode for SFS interactive query editor.
-
-The query editor uses the Recoll query language, documented here:
+The recollector uses the Recoll query language, documented here:
 https://www.lesbonscomptes.com/recoll/usermanual/webhelp/docs/RCL.SEARCH.LANG.html"
   (setq font-lock-defaults '(sfs--query-highlights)))
-
-
-(define-widget 'sfs-query 'default
-  "An editable text field for sfs queries.
-Note: In an `sfs-query' widget, the `%v' escape must be preceded
-by some other text in the `:format' string (if specified)."
-  :convert-widget 'widget-value-convert-widget
-  :keymap sfs-query-editor-mode-map
-  :format "%v"
-  :help-echo "M-TAB: complete field; RET: enter value"
-  :value ""
-  :prompt-internal 'widget-field-prompt-internal
-  :prompt-history 'widget-field-history
-  :prompt-value 'widget-field-prompt-value
-  :action 'widget-field-action
-  :validate 'widget-field-validate
-  :valid-regexp ""
-  :error "Field's value doesn't match allowed forms"
-  :value-create 'widget-field-value-create
-  :value-set 'widget-field-value-set
-  :value-delete 'widget-field-value-delete
-  :value-get 'widget-field-value-get
-  :match 'widget-field-match)
 
 (defvar sfs--logo-section)
 (defun sfs-insert-logo ()
   "Make SFS logo in query builder buffer."
   ;; (widget-insert (f-read-text "./assets/graffiti.txt"))
-  (let ((logo "--- S*fs : query editor ---")
+  (let ((logo "--- S*fs : recollector ---")
         top bot)
     (setq top (point))
     (widget-insert logo)
@@ -207,21 +236,20 @@ by some other text in the `:format' string (if specified)."
     (setq sfs--logo-section (sfs-section-create top bot))))
 
 
-
-(defvar sfs--query-editor-section)
-(defun sfs-make-section-query-editor ()
+(defvar sfs--recollector-section)
+(defun sfs-make-section-recollector ()
   "Make the query section in the sfs TUI."
   (let (top bot)
     (setq top (point))
     (setq sfs--editor-widget
-          (widget-create 'sfs-query
-                         :keymap sfs-query-editor-mode-map
+          (widget-create 'editable-field
+                         :keymap sfs-recollector-mode-map
                          :action (lambda (wid &rest ignore)
                                    (sfs-recoll (widget-value wid)))
-                         :notify (lambda (wid changed &rest ingore)
+                         :notify (lambda (wid changed &rest ignore)
                                    (setq sfs--query (widget-value wid)))))
     (setq bot (point))
-    (setq sfs--query-editor-section (sfs-section-create top bot))
+    (setq sfs--recollector-section (sfs-section-create top bot))
     (widget-create 'push-button
                    :notify (lambda (&rest ignore)
                              (sfs-recoll (widget-value sfs--editor-widget)))
@@ -229,53 +257,55 @@ by some other text in the `:format' string (if specified)."
     (widget-insert "(C-RET)\n")
     (widget-create 'push-button
                    :notify (lambda (&rest ignore)
+                             (call-interactively #'sfs-add-favorite))
+                   "Add to favorites")
+    (widget-insert "(C-c C-v)\n")
+    (widget-create 'push-button
+                   :notify (lambda (&rest ignore)
                              (call-interactively #'sfs-save-query))
-                   "Save query"))
-  (widget-insert "(C-c C-v)"))
+                   "Save to recollection.at.path")
+    (widget-insert "(C-c C-s)")))
 
-
-(defun sfs--make-query-editor-tui ()
+(defun sfs--make-recollector-tui ()
   "Write the tui buffer."
   (let ((buffer
-         (switch-to-buffer "*SFS*")))
+         (switch-to-buffer "*SFS: recollector*")))
     (with-current-buffer buffer
       (kill-all-local-variables)
       (remove-overlays)
       (let ((inhibit-read-only t))
         (erase-buffer))
       (sfs-insert-logo)
-      (sfs-make-section-query-editor)
-      (set-window-point (get-buffer-window "*SFS*")
-                        (sfs-section-top sfs--query-editor-section))
+      (sfs-make-section-recollector)
+      (set-window-point (get-buffer-window "*SFS: recollector*")
+                        (sfs-section-top sfs--recollector-section))
       (widget-setup)
-      (sfs-query-editor-mode)
+      (sfs-recollector-mode)
       (set-region-read-only (sfs-section-top sfs--logo-section)
                             (sfs-section-bot sfs--logo-section)))))
-
 
 (defun sfs--tui-run (f)
   "Call F in TUI context."
   (call-interactively f)
-  (sfs--make-query-editor-tui))
+  (sfs--make-recollector-tui))
 
-(defun sfs ()
+(defun sfs-recollect ()
   "Go to SFS start page."
   (interactive)
-  (sfs--make-query-editor-tui))
+  (sfs--make-recollector-tui))
 
-
-;;; Collections TUI
-(defun sfs--class-recents ()
-  "Make the class for recently touched files."
-  (let ((recent-collections '())
+;;; Recollections TUI
+(defun sfs--rec-recents ()
+  "Make the rec for recently touched files."
+  (let ((recent-recollections '())
         (recents
          `((day . ,(calendar-current-date))
            (week . ,(calendar-gregorian-from-absolute
-                       (+ (calendar-absolute-from-gregorian (calendar-current-date)) -7)))
+                     (+ (calendar-absolute-from-gregorian (calendar-current-date)) -7)))
            (month . ,(calendar-gregorian-from-absolute
-                        (+ (calendar-absolute-from-gregorian (calendar-current-date)) -30)))
+                      (+ (calendar-absolute-from-gregorian (calendar-current-date)) -30)))
            (year . ,(calendar-gregorian-from-absolute
-                       (+ (calendar-absolute-from-gregorian (calendar-current-date)) -365)))))
+                     (+ (calendar-absolute-from-gregorian (calendar-current-date)) -365)))))
         query-str day month year rtn-date)
     (dolist (elmt recents)
       (setq rtn-date (cdr elmt))
@@ -283,46 +313,48 @@ by some other text in the `:format' string (if specified)."
       (setq month (nth 0 rtn-date))
       (setq year (nth 2 rtn-date))
       (setq query-str (format "date:%d-%d-%d/" year month day))
-      (add-to-list 'recent-collections `(,(car elmt) . ,query-str)))
+      (push `(,(car elmt) . (,query-str)) recent-recollections))
 
-    recent-collections))
+    `(recents . ,recent-recollections)))
 
-(defun sfs--class-media ()
-  "Make the media class."
-  `((comics . ,(sfs--queries-or (sfs-split-and-prefix "ext" "djvu cbr cbz cb7 cbt cba")))
-    (books . ,(sfs--queries-or (sfs-split-and-prefix "ext" "ibooks pdf epub pbd djvu azw azw3 kf8 kfx fb2 mobi opf")))
-    (documents . ,(sfs--queries-or (sfs-split-and-prefix "ext" "pdf doc docx txt tex")))
-    (music . "mime:audio")
-    (video . "mime:video")
-    (text . "mime:text")))
+(defun sfs--rec-media ()
+  "Make the media rec."
+  `(media .
+          ((comics . (,(sfs--queries-or (sfs--split-and-prefix "ext" "djvu cbr cbz cb7 cbt cba"))))
+           (books . (,(sfs--queries-or (sfs--split-and-prefix "ext" "ibooks pdf epub pbd djvu azw azw3 kf8 kfx fb2 mobi opf"))))
+           (documents . (,(sfs--queries-or (sfs--split-and-prefix "ext" "pdf doc docx txt tex"))))
+           (music . ("mime:audio"
+                     ,(sfs--queries-or (sfs--split-and-prefix "ext" "mp3 aac"))
+                     (lossless ,(sfs--queries-or (sfs--split-and-prefix "ext" "wav flac aiff")))
+                     (lossy ,(sfs--queries-or (sfs--split-and-prefix "ext" "mp3 aac")))))
+           (video . ("mime:video"))
+           (text . ("mime:text")))))
 
-(defun sfs--class-all ()
-  `(,@(sfs--class-media)
-    ,@sfs-class-favs
-    ,@(sfs--class-recents)))
+(defun sfs--insert-heading (heading depth)
+  "SFS insert heading at depth."
+  (dotimes (n depth)
+    (insert "*"))
+  (insert (concat " " heading "\n")))
 
-(defun sfs--insert-class-section (class id)
-  "Make the section for CLASS with LABEL in the sfs class TUI."
-  ;; (sfs--insert-section-label heading)
-  (insert "* class:" id "\n")
-  (dolist (elmt class)
-    (insert (concat "** class:" (symbol-name (car elmt)) "\n"))
-    (insert (concat "*** " (cdr elmt)) "\n")))
+(defun sfs--insert-rec-section (rec depth)
+  "Make the section for REC with LABEL in the sfs rec TUI."
+  (if rec
+      (cond
+       ((listp rec)
+        (sfs--insert-rec-section (first rec) (+ depth 1))
+        (sfs--insert-rec-section (rest rec) depth))
+       ((symbolp rec)
+        (sfs--insert-heading (concat "rec:" (symbol-name rec)) depth))
+       (t (sfs--insert-heading rec (+ depth 1))))
+    nil))
 
-(defun sfs-insert-set (set-id)
-  "Insert the set associated with SET-ID at point."
-  (interactive "sEnter an sfs set: ")
-  (insert (alist-get set-id (sfs--class-all))))
-
-(defvar sfs-collections-mode-map)
-(setq sfs-collections-mode-map
+(defvar sfs-recollections-mode-map)
+(setq sfs-recollections-mode-map
       (let ((map (make-sparse-keymap)))
-        (define-key map (kbd "<C-return>") 'sfs-fetch-collection-at-point)
+        (define-key map (kbd "<return>") 'sfs-fetch-recollection-at-point)
         map))
-(define-derived-mode sfs-collections-mode outline-mode "SFS Collections"
-  "Mode for SFS interactive collections interface.")
-  ;; (outline-mode)
-
+(define-derived-mode sfs-recollections-mode outline-mode "SFS Recollections"
+  "Mode for SFS interactive recollections interface.")
 
 (defface sfs--read-only '((default . (:background "beige")))
   "Face for `my-read-only-region'")
@@ -345,31 +377,29 @@ Read-only text is given the face `sfs--read-only'."
       (remove-text-properties begin end '(read-only t))
       (remove-overlays begin end 'sfs--type 'read-only))))
 
-(defcustom sfs-collector-classes '((sfs-class-favs     "favorites")
-                                   (sfs--class-recents "recents")
-                                   (sfs--class-media  "media"))
-  "A list of set classes to be displayed in the SFS collector TUI."
-  :type  'sexp
-  :group 'sfs)
-
-(defun sfs--make-collections-tui ()
+(defun sfs--make-recollections-tui ()
   "Write the tui buffer."
-  (let ((buffer (switch-to-buffer "*SFS Collections*")))
+  (let ((buffer (switch-to-buffer "*SFS Recollections*")))
+    (setq sfs-recs `((favorites . ,sfs-favorites)
+                     sfs--rec-recents
+                     sfs--rec-media))
     (with-current-buffer buffer
       (erase-buffer)
       (push-mark)
       ;;TODO tags section
-      (dolist (el sfs-collector-classes)
-        (if (symbol-function (first el))
-            (sfs--insert-class-section (funcall (first el)) (second el))
-          (sfs--insert-class-section (symbol-value (first el)) (second el))))
-      (sfs--insert-class-section nil "scratch")
+      (dolist (el sfs-recs)
+        (cond ((listp el)
+               (sfs--insert-rec-section el 0))
+              ((symbol-function el)
+               (sfs--insert-rec-section (funcall el) 0))
+              (t (sfs--insert-rec-section (symbol-value el) 0))))
+      (insert "* rec:custom")
       (push-mark)
       (insert "\n")
       ;; (sfs--read-only-region (mark)
       ;;                        (progn (pop-mark)
       ;;                               (mark)))
-      (sfs-collections-mode))))
+      (sfs-recollections-mode))))
 
 (defun sfs--heading-depth (heading)
   (if (string-equal (substring heading 0 1) "*")
@@ -432,29 +462,32 @@ Read-only text is given the face `sfs--read-only'."
                    (string-join (mapcar #'sfs--tree-to-query (rest tree))
                                 (concat " " root " "))
                    ")"))
-          ;; ((string= (first (split-string root ":"))
-          ;;           "set")
-          ;;  (alist-get (intern (second (split-string root ":")))
-          ;;             (sfs--class-all)))
-          ;; also let people search entire classes as the union of its sets
           ((string= (first (split-string root ":"))
-                    "class")
+                    "rec")
            (concat "("
                    (string-join (mapcar #'sfs--tree-to-query (rest tree))
                                 " OR ")
                    ")"))
           (t root))))
 
-(defun sfs-fetch-collection-at-point ()
-  "Fetch the collection under the heading at point."
+(defun sfs-fetch-recollection-at-point ()
+  "Fetch the recollection under the heading at point."
   (interactive)
   (sfs-recoll (sfs--tree-to-query (sfs--heading-at-point-to-tree))))
 
-(defun sfs-collections ()
+(defun sfs-save-recollection-at-point (id)
+  "Save the recollection at point to rec ID."
+  (interactive "sEnter id path: ")
+  (let ((query-str (sfs-fetch-recollection-at-point)))
+    (if (alist-get id sfs-recs)
+        (setf (alist-get (intern id) sfs-recs)
+              `(,query-str))
+      (push `(,(intern id) ,(widget-value sfs--editor-widget)) sfs-favorites))))
+
+(defun sfs-recollections ()
   "Go to SFS start page."
   (interactive)
-  (sfs--make-collections-tui))
-
+  (sfs--make-recollections-tui))
 
 (provide 'sfs-tui)
 ;;; sfs-tui.el ends here
