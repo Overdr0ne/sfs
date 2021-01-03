22ffc8c0f11e6766e835b7d0bbc861cf6c851e23
Overdr0ne <0v3rdr0n3@gmail.com>
2021-01-02 21:15:18 -0500

    Added function to fix keybindings for evil users

---
+++ evil-collection-sfs.el
@@ -0,0 +1,13 @@
+(defun evil-collection-sfs-setup ()
+  (evil-collection-define-key '(normal insert) 'sfs-research-mode-map
+    (kbd "C-q")        'quit-window
+    (kbd "<C-return>") 'widget-field-activate
+    (kbd "C-r")        'sfs--record-query)
+  (evil-collection-define-key 'normal 'sfs-recollect-mode-map
+    (kbd "<return>") 'sfs-recollect-at-point
+    (kbd "q")        'quit-window)
+  (evil-collection-define-key 'normal 'sfs-represent-mode-map
+    (kbd "q")        'quit-window)
+  (evil-collection-define-key 'normal 'sfs-redir-mode-map
+    (kbd "<C-return>") 'sfs-represent
+    (kbd "q") 	       'quit-window))
