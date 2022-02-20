d5898d345138b42bb3cf489e41738ba1ab9ee065
Overdr0ne <0v3rdr0n3@gmail.com>
2022-02-20 06:51:27 -0700

    tag: remove ivy dependency, just use completing-read

--- sfs-tag.el
+++ sfs-tag.el
@@ -19,50 +19,45 @@
 ;;
 ;;; Code:
 
-(require 'ivy)
+(require 'dash)
 
 (defun sfs-tag-collect-tags ()
   "Collect existing tags for file at POINT."
   (let (attr)
     (setq attr (-> (format "getfattr --absolute-names %s"
-                          (shell-quote-argument (dired-get-filename)))
-                  (shell-command-to-string)))
+                           (shell-quote-argument (dired-get-filename)))
+                   (shell-command-to-string)))
     (setq attr (dired-replace-in-string "#.*" "" attr))
     (split-string attr)))
 
 (defun sfs-tag-collect-vals (tag)
-  (-> (format "getfattr --absolute-names --only-values -n %s %s"
-             (shell-quote-argument tag) (shell-quote-argument (dired-get-filename)))
-     (shell-command-to-string)
-     (split-string)))
-
-(defun sfs-tag-set-action (tag)
-  "Set tag for file at POINT."
-  (ivy-read "Value: " (sfs-tag-collect-vals tag)
-            :action (lambda (candidate)
-                      (shell-command-to-string
-                       (format "setfattr -n %s -v %s %s" tag candidate
-                               (dired-get-filename))))))
+  (-> (format "getfattr --absolute-names --only-values -n %s %s 2>/dev/null"
+              tag (shell-quote-argument (dired-get-filename)))
+      (shell-command-to-string)
+      (split-string)))
 
 (defun sfs-tag-set ()
   "Interactively tag the file at POINT using extended attributes."
   (interactive)
-  (ivy-read "Tag: " (sfs-tag-collect-tags)
-            :action 'sfs-tag-set-action))
+  (let ((tag (completing-read "Tag: " (sfs-tag-collect-tags))))
+    (shell-command-to-string
+     (format "setfattr -n %s -v %s %s" tag
+	     (completing-read "Value: " (sfs-tag-collect-vals tag))
+             (dired-get-filename)))))
 
 (defun sfs-tag-collect-dump ()
   "Collect existing tags for file at POINT."
   (let (attr)
     (setq attr (-> (format "getfattr --absolute-names -d %s"
-                          (shell-quote-argument (dired-get-filename)))
-                  (shell-command-to-string)))
+                           (shell-quote-argument (dired-get-filename)))
+                   (shell-command-to-string)))
     (setq attr (dired-replace-in-string "#.*" "" attr))
     (split-string attr)))
 
 (defun sfs-tag-get ()
   "Get all tag info for file at POINT using extended attributes."
   (interactive)
-  (ivy-read "Tag: " (sfs-tag-collect-dump)))
+  (completing-read "Tag: " (sfs-tag-collect-tags)))
 
 (provide 'sfs-tag)
 ;;; sfs-tag.el ends here
