a60b015d5674dfaf481322b34556bbaf2517375c
Overdr0ne <0v3rdr0n3@gmail.com>
2022-02-20 17:25:35 -0700

    tag: add command to remove tags
    Also added initial input for setting tags and messaging after tags are set.

--- sfs-tag.el
+++ sfs-tag.el
@@ -36,19 +36,38 @@
       (shell-command-to-string)
       (split-string)))
 
+(defcustom sfs-tag-initial-input "user."
+  "A list of set recs to be displayed in the SFS collector TUI."
+  :type  'string
+  :group 'sfs)
+
 (defun sfs-tag-set ()
   "Interactively tag the file at POINT using extended attributes."
   (interactive)
-  (let ((tag (completing-read "Tag: " (sfs-tag-collect-tags))))
+  (let* ((tag (completing-read "Tag: " (sfs-tag-collect-tags) nil nil sfs-tag-initial-input))
+	 (value (completing-read "Value: " (sfs-tag-collect-vals tag)))
+	 (filename (dired-get-filename)))
     (shell-command-to-string
-     (format "setfattr -n %s -v %s %s" tag
-	     (completing-read "Value: " (sfs-tag-collect-vals tag))
-             (dired-get-filename)))))
+     (format "setfattr -n %s -v %s %s" tag value filename))
+    (message (format "Set tag %s to %s on %s." tag value filename))))
 
 (defun sfs-tag-get ()
   "Get all tag info for file at POINT using extended attributes."
   (interactive)
-  (completing-read "Tag: " (sfs-tag-collect-tags)))
+  (let* ((tag (completing-read "Tag: " (sfs-tag-collect-tags)))
+	 (filename (dired-get-filename))
+	 (value (shell-command-to-string
+		 (format "getfattr --absolute-names --only-values -n %s %s" tag filename))))
+    (message "Tag %s has value %s on %s." tag value filename)))
+
+(defun sfs-tag-rm ()
+  "Remove tag from file at POINT."
+  (interactive)
+  (let ((tag (completing-read "Remove tag: " (sfs-tag-collect-tags)))
+	(filename (dired-get-filename)))
+    (shell-command-to-string
+     (format "setfattr -x %s %s" tag filename))
+    (message (format "Removed tag %s from %s." tag filename))))
 
 (provide 'sfs-tag)
 ;;; sfs-tag.el ends here
